52ac145293e71271ee654e9f1a8b6bcee42c7a96
NIJHUM17 <tahsinanijum5199@gmail.com>
2022-10-13 10:59:26 +0600

    Basic Selenium

--- TestProject/Testing.py
+++ TestProject/Testing.py
@@ -1,28 +1,41 @@
+
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-'''first_name = "Tahsin Adiba"
-last_name = "Nijhum"
+
+first_name = "Tahsin"
+last_name = "Adiba"
 email = "tahsinanijum5199@gmail.com"
-password = "123Tahsin"'''
+password = "123Tahsin"
 
-#url = "http://automationpractice.com/index.php
+driver = webdriver.Chrome(service=Service('D:\Python\pythonProject\BasicSelenium\drivers\chromedriver.exe'))
+driver.get("https://demo.opencart.com")
+print(driver.title)
 
-pathdriver = Service("C:\Users\USER\Downloads\Path\chromedriver.exe")
-driver = webdriver.Chrome(service=pathdriver)
-driver.get("http://automationpractice.com/index.php")
-
-driver.find_element().click()
-driver.find_element().click()
+driver.find_element("xpath", "//*[@id='top']/div[2]/div[2]/ul/li[2]/div/a/span").click()
 time.sleep(3)
-
-element = driver.find_element("")
+driver.find_element("xpath", "//*[@id='top']/div[2]/div[2]/ul/li[2]/div/ul/li[1]/a").click()
+time.sleep(3)
+element = driver.find_element("id", "input-firstname")
 element.send_keys(first_name)
 
-element = driver.find_element("")
+element = driver.find_element("id", "input-lastname")
 element.send_keys(last_name)
 
+element = driver.find_element("id", "input-email")
+element.send_keys(email)
 
+element = driver.find_element("id", "input-password")
+element.send_keys(password)
 
+driver.find_element("xpath", "//*[@id='input-newsletter-yes']").click()
+time.sleep(1)
+driver.find_element("xpath", "//*[@id='form-register']/div/div/div/input").click()
+time.sleep(1)
+
+driver.close()
+driver.quit()
 
 
 
